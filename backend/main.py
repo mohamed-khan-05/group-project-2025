7e81decfa6d4ed5320e5f81715d92a2129a767bc
Mohamed <mohamedkhan050124@gmail.com>
2025-03-05 20:23:00 +0200

    update main.py

--- backend/main.py
+++ backend/main.py
@@ -14,4 +14,7 @@ app.register_blueprint(Wishlist_bp, url_prefix="/wishlist")
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=port, debug=True)
