8440b0e372044d547f872654c80c86c890561a2f
Mohamed <mohamedkhan050124@gmail.com>
2025-03-07 13:51:13 +0200

    logout

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  const navigate = useNavigate();
+  const logout = () => {
+    const confirmLogout = window.confirm("Are you sure you want to logout?");
+    if (confirmLogout) {
+      localStorage.removeItem("user_id");
+      navigate("/");
+    }
+  };
+  return (
+    <div>
+      Homepage
+      <button
+        className="border-1 p-1 m-5"
+        onClick={() => {
+          logout();
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default Homepage;
