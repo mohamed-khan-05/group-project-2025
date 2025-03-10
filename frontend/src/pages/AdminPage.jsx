43959387ac7c06ed4c22dc59fcb9509fab280535
Shaunak Debba <Shaunakdebba13@gmail.com>
2025-03-10 09:59:36 +0200

    update

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,11 +1,28 @@
 import React, { useEffect } from "react";
 import AdminMenu from "../components/AdminMenu";
+import { useNavigate } from "react-router-dom";
 
 const AdminPage = () => {
+  const navigate = useNavigate()
+  const logout = () => {
+    const confirmLogout = window.confirm("Are you sure you want to logout?");
+    if (confirmLogout) {
+      localStorage.removeItem("user_id");
+      navigate("/");
+    }
+  };
   return (
     <div>
       AdminPage
       <AdminMenu />
+      <button
+        className="border-1 p-1 m-5"
+        onClick={() => {
+          logout();
+        }}
+      >
+        Logout
+      </button>
     </div>
   );
 };
