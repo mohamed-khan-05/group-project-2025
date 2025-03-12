760dbd48c3775d4d61f8ba4e9672182179c9cabd
Shaunak Debba <Shaunakdebba13@gmail.com>
2025-03-12 14:07:38 +0200

    User books

--- backend/routes/Books.py
+++ backend/routes/Books.py
@@ -6,3 +6,24 @@ from models import db, Book, Review, User
 from werkzeug.utils import secure_filename
 
 Books_bp = Blueprint("Books_bp", __name__)
+
+# Define API routes under the Blueprint
+@Books_bp.route('/api/books', methods=['GET'])
+def get_books():
+    books = Book.query.filter(Book.quantity > 0).all()  # Fetch books where quantity > 0
+    book_list = []
+    
+    for book in books:
+        book_list.append({
+            'id': book.id,
+            'title': book.title,
+            'author': book.author,
+            'price': str(book.price),  # Convert price to string for JSON serialization
+            'image': book.image,
+            'quantity': book.quantity,
+            'description': book.description
+        })
+    
+    return jsonify(book_list)
+
+
--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const BookCard = () => {
-  return <div>BookCard</div>;
+const BookCard = ({book}) => {
+  return <div key={book.id} className="book-item">
+  <img src={book.image} alt={book.title} className="book-image" />
+  <h2>{book.title}</h2>
+  <p>{book.author}</p>
+  <p>{book.description}</p>
+  <p><strong>Price:</strong> ${book.price}</p>
+  <button>Add to Cart</button>
+</div>;
 };
 
 export default BookCard;
