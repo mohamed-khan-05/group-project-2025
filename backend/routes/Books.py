c3126832ab2b34cb000764f1a08e71a7d7668b41
Mohamed <mohamedkhan050124@gmail.com>
2025-03-12 11:26:08 +0200

    added menus

--- backend/routes/Books.py
+++ backend/routes/Books.py
@@ -6,3 +6,38 @@ from models import db, Book, Review, User
 from werkzeug.utils import secure_filename
 
 Books_bp = Blueprint("Books_bp", __name__)
+
+@Books_bp.route("/addbook", methods=["POST"])
+def addbook():
+    title = request.form.get("title")
+    description = request.form.get("description")
+    author = request.form.get("author")
+    category = request.form.get("category")
+    quantity = request.form.get("quantity", type=int)
+    price = request.form.get("price", type=float)
+    discount = request.form.get("discount", type=float, default=0)
+    image = request.files.get("image")
+
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+    if image:
+        filename = secure_filename(image.filename)
+        name, extension = os.path.splitext(filename)
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        image_filename = f"{timestamp}_{name}{extension}"
+        image.save(os.path.join(app.config['UPLOAD_FOLDER'], image_filename))
+    else:
+        image_filename = "BookPlaceholder.png"
+
+    new_book = Book(
+        title=title,
+        description=description,
+        author=author,
+        category=category,
+        quantity=quantity,
+        price=price,
+        discount=discount,
+        image=image_filename
+    )
+    db.session.add(new_book)
+    db.session.commit()
+    return jsonify({"status":"200"})
--- frontend/src/components/AdminMenu.jsx
+++ frontend/src/components/AdminMenu.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AdminMenu = () => {
-  return <div>AdminMenu</div>;
+  const navigate = useNavigate();
+
+  const logout = () => {
+    const confirmLogout = window.confirm("Are you sure you want to logout?");
+    if (confirmLogout) {
+      localStorage.removeItem("user_id");
+      navigate("/");
+    }
+  };
+
+  return (
+    <div className="flex justify-evenly border p-2 mb-10 bg-amber-200">
+      <h1
+        className="bg-white py-1 px-3 cursor-pointer"
+        onClick={() => {
+          navigate("/adminpage");
+        }}
+      >
+        Home
+      </h1>
+      <h1
+        className="bg-white py-1 px-3 cursor-pointer"
+        onClick={() => {
+          navigate("/managebooks");
+        }}
+      >
+        Manage Books
+      </h1>
+      <h1 className="line-through">Orders</h1>
+      <h1 className="line-through">Profile</h1>
+      <button
+        className="border-1 bg-white py-1 px-3"
+        onClick={() => {
+          logout();
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default AdminMenu;
