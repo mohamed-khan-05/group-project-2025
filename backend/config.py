8d9195f28fb087013b1ec99108d1cdde7e2cb98f
unknown <mohamedkhan050124@gmail.com>
2025-03-21 13:41:11 +0200

    'save'

--- backend/config.py
+++ backend/config.py
@@ -7,21 +7,13 @@ import os
 
 app = Flask(__name__)
 FRONTEND_URL = os.getenv("FRONTEND_URL", "https://devdynamos-bookstore.netlify.app")
-CORS(app, resources={r"/*": {"origins": FRONTEND_URL}}, supports_credentials=True)
+CORS(app)
 
 UPLOAD_FOLDER = 'uploads/books'
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///bookstore.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
-
-@app.after_request
-def add_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = FRONTEND_URL
-    response.headers["Access-Control-Allow-Credentials"] = "true"
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
-    return response
 
 @app.route('/uploads/books/<filename>')
 def uploaded_book_file(filename):
