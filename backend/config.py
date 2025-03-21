ca9a6e7e8caa27703636b0f0debe9f22bb03d00a
unknown <mohamedkhan050124@gmail.com>
2025-03-21 15:36:58 +0200

    'save'

--- backend/config.py
+++ backend/config.py
@@ -21,25 +21,18 @@ def uploaded_book_file(filename):
 db = SQLAlchemy(app)
 
 from routes.LoginSignup import LoginSignup_bp
-CORS(LoginSignup_bp)
 app.register_blueprint(LoginSignup_bp, url_prefix="/auth")
 from routes.Books import Books_bp
-CORS(Books_bp)
 app.register_blueprint(Books_bp, url_prefix="/books")
 from routes.Reviews import Reviews_bp
-CORS(Reviews_bp)
 app.register_blueprint(Reviews_bp, url_prefix="/reviews")
 from routes.Wishlist import Wishlist_bp
-CORS(Wishlist_bp)
 app.register_blueprint(Wishlist_bp, url_prefix="/wishlist")
 from routes.Cart import Cart_bp
-CORS(Cart_bp)
 app.register_blueprint(Cart_bp, url_prefix="/cart")
 from routes.Orders import Orders_bp
-CORS(Orders_bp)
 app.register_blueprint(Orders_bp, url_prefix="/orders")
 from routes.Filter import Filter_bp
-CORS(Filter_bp)
 app.register_blueprint(Filter_bp, url_prefix="/filter")
 
 @app.after_request
