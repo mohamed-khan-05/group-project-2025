2558221bc8743371186889fdd131fa33317a280c
unknown <mohamedkhan050124@gmail.com>
2025-03-21 11:40:45 +0200

    cors

--- backend/config.py
+++ backend/config.py
@@ -7,10 +7,15 @@ import os
 
 app = Flask(__name__)
 FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
-CORS(app, resources={r"/*": {"origins": [FRONTEND_URL, "https://devdynamos-bookstore.netlify.app"]}}, 
-     supports_credentials=True)
+CORS(app, resources={r"/*": {
+    "origins": [FRONTEND_URL, "https://devdynamos-bookstore.netlify.app"],
+    "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    "allow_headers": ["Content-Type", "Authorization"],
+    "supports_credentials": True
+}})
 
 UPLOAD_FOLDER = 'uploads/books'
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///bookstore.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
