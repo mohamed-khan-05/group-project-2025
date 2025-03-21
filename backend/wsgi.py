6758b28eb76882e7e41a08860afa6aa2a6a334d5
unknown <mohamedkhan050124@gmail.com>
2025-03-21 15:09:02 +0200

    'save'

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,4 +1,13 @@
 from config import app, db
+import logging
+from flask import request
+
+logging.basicConfig(level=logging.INFO)
+
+@app.before_request
+def log_request_info():
+    logging.info(f"Incoming request: {request.method} {request.path}")
+    logging.info(f"Headers: {request.headers}")
 
 with app.app_context():
     db.create_all()
